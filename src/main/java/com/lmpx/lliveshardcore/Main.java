2f3f1def70a1974f25fa4e462e17f2055391fad1
LIMPIX31 <dl.qpoint@gmail.com>
2021-03-24 13:23:29 +0300

    Plugin enabling and config file added

--- src/main/java/com/lmpx/lliveshardcore/Main.java
+++ src/main/java/com/lmpx/lliveshardcore/Main.java
@@ -1,10 +1,34 @@
 package com.lmpx.lliveshardcore;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.IOException;
 
 public class Main extends JavaPlugin {
     @Override
     public void onEnable() {
+        File config = new File(getDataFolder() + File.separator + "config.yml");
+        if (!config.exists()) {
+            getLogger().info(ChatColor.BLUE + "Creating default config file");
+            getConfig().options().copyDefaults(true);
+            saveDefaultConfig();
+        }
+
+        try {
+            Functions.createMessagesFile(this);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (Bukkit.getPluginManager().isPluginEnabled("PlaceholderAPI")) {
+
+        } else {
+            getLogger().severe(ChatColor.RED + "Required PlaceholderAPI");
+            Bukkit.getPluginManager().disablePlugin(this);
+        }
 
     }
 
