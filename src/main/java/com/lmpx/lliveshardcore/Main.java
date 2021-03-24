0cdc58d3c4b236d03815e23dadc293868f9b420e
LIMPIX31 <dl.qpoint@gmail.com>
2021-03-24 12:47:15 +0300

    init

---
+++ src/main/java/com/lmpx/lliveshardcore/Main.java
@@ -0,0 +1,15 @@
+package com.lmpx.lliveshardcore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
